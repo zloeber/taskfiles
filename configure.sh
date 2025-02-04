ff2dd56e2ca44106f31faad5d530b98be67a0a0b
Zachary Loeber <zloeber@gmail.com>
2025-02-04 14:29:16 -0600

    fix: update to using uv instead of pip

--- configure.sh
+++ configure.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-# install mise if not already installed to local path
-if [ ! -f ~/.local/bin/mise ]; then
-    curl https://mise.run | sh
+# check for mise
+if ! command -v mise &>/dev/null; then
+    echo "Please install mise first (run 'curl https://mise.run | sh')"
+    echo ""
+    echo "Load mise into your path (add to your ~/.zshrc profile to always be available): "
+    echo '   echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc'
+    exit 1
+else
+    eval "$(mise activate bash)"
 fi
-
-eval "$(~/.local/bin/mise activate zsh)"
 
 mise install -y
 
 task python:venv python:install
-
-echo ""
-echo "Load mise into your path (add to your ~/.zshrc profile to always be available): "
-echo '   echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc'
